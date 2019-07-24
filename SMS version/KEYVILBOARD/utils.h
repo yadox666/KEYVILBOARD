dd841ae5ed05c29e7ba609bcc877f71d236e8895
Yadox <sti@soportec.com>
2019-07-24 14:55:07 +0200

    Update utils.h

--- SMS version/KEYVILBOARD/utils.h
+++ SMS version/KEYVILBOARD/utils.h
@@ -1,8 +1,9 @@
-
 #include <Arduino.h>
 
 void SMSSerialFlush();
 String readResponse();
 void collectDebugInfo();
+void collectSimInfo();
+String getModemSignal();
 void sendSMSMessage(String txt);
 String getValue(String data, String separator, int index);
