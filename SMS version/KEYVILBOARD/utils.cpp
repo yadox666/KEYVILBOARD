03fa6d4d899493f6b0bdf23ad5842b6ff9b0735e
Yadox <sti@soportec.com>
2019-07-24 14:54:35 +0200

    Update utils.cpp

--- SMS version/KEYVILBOARD/utils.cpp
+++ SMS version/KEYVILBOARD/utils.cpp
@@ -1,4 +1,3 @@
-
 #include "utils.h"
 #include "globals.h"
 #include <Arduino.h>
@@ -8,92 +7,108 @@ void SMSSerialFlush(){
   while(SMSSERIAL.available() > 0){
     //int byte = SMSSERIAL.read();
     SMSSERIAL.readString();
-#ifdef DEBUG
-    //Serial.print((char)byte);
-#endif
+    #ifdef DEBUG
+        //Serial.print((char)byte);
+    #endif
   }
 }   
+
 
 String readResponse(){
   delay(500);
   String res;
   int TIMEOUT = 10 *10; // wait 10 seconds
   int incomingByte  = 0;
-  while (!SMSSERIAL.available()) 
-  {
+  while (!SMSSERIAL.available()) {
     delay(100);
     TIMEOUT--;
     if (TIMEOUT == 0){
-#ifdef DEBUG
-      Serial.println(F("Timeout reading response"));
-#endif
+      #ifdef DEBUG
+            Serial.println(F("Timeout reading response"));
+      #endif
       return "";
     }
   }
-  
-  while (SMSSERIAL.available()) 
-  {
+  while (SMSSERIAL.available()) {
     incomingByte  = SMSSERIAL.read();
-    if (incomingByte == -1){ // this should never happen 
-#ifdef DEBUG
-      Serial.println("There is no data to read from SMSSERIAL");
-#endif
+    if (incomingByte == -1){              // this should never happen 
+      #ifdef DEBUG
+        Serial.println("There is no data to read from SMSSERIAL");
+      #endif
       break; 
     }
-#ifdef DEBUG
-    Serial.write(char(incomingByte));
-#endif
+    #ifdef DEBUG
+      Serial.write(char(incomingByte));
+    #endif
     res += char(incomingByte);
   }
   return res;
 }
 
+
 void collectDebugInfo(){
-  SMSSERIAL.println(F("AT+CMEE=1")); //enable debug
+  SMSSERIAL.println(F("AT+CMEE=1")); // Enable extended debug messages
   readResponse();
   Serial.println(F("--")); 
   
-  SMSSERIAL.println(F("AT")); //Once the handshake test is successful, it will back to OK
+  SMSSERIAL.println(F("AT")); // Once the handshake test is successful, it will back to OK
   readResponse();
   Serial.println(F("--"));
   
-  SMSSERIAL.println(F("AT+CSQ")); //Signal quality test, value range is 0-31 , 31 is the best
+  SMSSERIAL.println(F("AT+CMGL=\"ALL\"")); // List Received Messages in Text Mode 
   readResponse();
   Serial.println(F("--"));
-  
-  SMSSERIAL.println(F("AT+CCID")); //Read SIM information to confirm whether the SIM is plugged
+}
+
+
+void collectSimInfo(){
+  SMSSERIAL.println(F("AT+CCID")); // Read SIM information to confirm whether the SIM is plugged
   readResponse();
   Serial.println(F("--"));
 
-  //read stored sms
-  SMSSERIAL.println(F("AT+CMGL=\"ALL\"")); 
-  readResponse();
-  Serial.println(F("--"));
-
-  //check card is properly inserted
-  SMSSERIAL.println(F("AT+CPIN?")); 
+  SMSSERIAL.println(F("AT+CPIN?")); // Check card is properly inserted and PIN ready
   String res = readResponse();
+  #ifdef DEBUG
+    if(res.indexOf(F("CPIN: READY")) <= 0){
+      Serial.println(F("Check SIM card!"));
+    }
+  #endif
+}
 
-#ifdef DEBUG
-  if(res.indexOf(F("CPIN: READY")) <= 0){
-    Serial.println(F("The SIM card is not ready to be used. Check you inserted id correctly"));
+
+String getModemSignal(){
+  SMSSERIAL.println(F("AT+CSQ"));    // Signal quality test, value range is 0-31 , 31 is the best
+  String sgn = readResponse();
+  // Parse only first digits from signal response
+  int startPos = 0;
+  do {
+    if (isdigit(sgn.charAt(startPos)))
+        break;
   }
-#endif
+  while (++startPos < sgn.length());
+  int endPos = startPos;
+  do {
+    if (!isdigit(sgn.charAt(endPos)))
+        break;
+  }
+  while (++endPos < sgn.length());
+  String cellsignal = sgn.substring(startPos, endPos);  
+  return cellsignal;
 }
 
 
 // Returns 1 if SMS was sucessfully sent or 0 if not
 void sendSMSMessage(String txt){
-  //return;
-#ifdef DEBUGWITHOUTSIM
-  Serial.print(F("Sending SMS: "));
-  Serial.println(txt);
-  return;
-#endif
-  SMSSERIAL.println(F("AT+CMGF=1"));
+  #ifdef DEBUGWITHOUTSIM
+    Serial.print(F("Sending SMS: "));
+    Serial.println(txt);
+    return;
+  #endif
+  
+  SMSSERIAL.println(F("AT+CMGF=1"));                      //operating mode: SMS text mode
   delay(2);
 
-  SMSSERIAL.println("AT+CMGS=\"" LEAK_PHONE_NUMBER "\""); //phone number with land code 
+  SMSSERIAL.println("AT+CMGS=\"" LEAK_PHONE_NUMBER "\"");
   delay(2);
 
   SMSSERIAL.write(txt.c_str());
@@ -104,6 +119,7 @@ void sendSMSMessage(String txt){
   SMSSERIAL.write((char)26);
   delay(2);
 }
+
 
 String getValue(String data, String separator, int index){
   int found = 0;
