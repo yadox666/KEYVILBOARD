4bcadb923b2a2550c2aaf69bf23433ab6b51095b
Yadox <sti@soportec.com>
2019-07-25 18:13:30 +0200

    Update utils.h

--- SMS version/KEYVILBOARD/utils.h
+++ SMS version/KEYVILBOARD/utils.h
@@ -4,6 +4,7 @@ void SMSSerialFlush();
 String readResponse();
 void collectDebugInfo();
 void collectSimInfo();
+void configureModem();
 String getModemSignal();
 void sendSMSMessage(String txt);
 String getValue(String data, String separator, int index);
